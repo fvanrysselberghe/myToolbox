dddfb320e3712266e14918cb9446ae193c89cfe5
fvanrysselberghe <filip@byte-atelier.be>
2024-10-07 21:31:13 +0200

    create base deployment scripts

---
+++ deploy.ps1
@@ -0,0 +1,46 @@
+#pre-requisite
+#winget install --id=Microsoft.PowerShell  -e
+
+#essentials
+winget install --id=Bitdefender.Bitdefender  -e
+winget install --id=7zip.7zip -e  
+winget install --id=Mozilla.Thunderbird.eu -e  
+winget install --id=Mozilla.Firefox -e  
+winget install --id=JohnMacFarlane.Pandoc -e  
+winget install --id=MiKTeX.MiKTeX -e  
+winget install --id=Hugo.Hugo -e  
+winget install --id=Microsoft.PowerToys -e
+winget install --id=Git.Git -e  
+winget install --id=Microsoft.WindowsTerminal -e  
+
+# Extras
+winget install --id=Xmind.Xmind.8 -e  
+winget install --id=JGraph.Draw -e
+winget install --id=Inkscape.Inkscape -e  
+winget install --id=Audacity.Audacity -e 
+winget install --id=VideoLAN.VLC  -e
+
+# Development
+winget install --id=Microsoft.VisualStudioCode -e
+winget install --id=Neovim.Neovim -e 
+winget install --id=WinMerge.WinMerge -e  
+
+## C++
+winget install --id=Ninja-build.Ninja -e
+winget install --id=Kitware.CMake -e  
+winget install --id=LLVM.LLVM -e
+winget install --id=LLVM.clangd -e  
+winget install --id=LLVM.ClangFormat -e  
+ 
+# C#
+winget install --id=icsharpcode.ILSpy -e
+
+# Python
+winget install --id=Python.Python.3.12 -e 
+
+# Other 
+winget install --id=Iterate.Cyberduck  -e 
+
+# Fun
+winget install --id=Garmin.BaseCamp -e 
+winget install --id=ArduinoSA.IDE.stable  -e
---
+++ deploy_vscode_extensions.ps1
@@ -0,0 +1,30 @@
+
+# Essentials
+code --install-extension vscodevim.vim
+
+# C++
+code --install-extension vadimcn.vscode-lldb
+code --install-extension llvm-vs-code-extensions.vscode-clangd
+
+# C#
+code --install-extension ms-dotnettools.vscode-dotnet-runtime
+code --install-extension adrianwilczynski.user-secrets
+
+# Docker
+code --install-extension ms-azuretools.vscode-docker
+
+
+# Python
+code --install-extension ms-python.python
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.vscode-pylance
+
+# Powershell
+code --install-extension ms-vscode.powershell
+
+# Fun
+code --install-extension James-Yu.latex-workshop
+code --install-extension piersdeseilligny.betterfountain
+
+# Elixir
+code --install-extension JakeBecker.elixir-ls
